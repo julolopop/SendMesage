6f635f808e91ca0ddf5793483bfd9abd3bad278e
juanma <julolopop@gmail.com>
2017-09-28 10:42:22 +0200

    Actividad completa de enviar y recibir mensage

--- app/src/main/java/com/example/usuario/sendmensagge/SendMensaggerActivity.java
+++ app/src/main/java/com/example/usuario/sendmensagge/SendMensaggerActivity.java
@@ -4,32 +4,74 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
-
+/**
+ * @author  Juan Manuel Diaz Ortiz
+ * @version v0.2
+ * <p>Esta clase manda un mensage a la clase ViewMesaggeActivity</p>
+ *
+ * <ul>
+ *     <li>Concepto Context</li>
+ *     <li>Paso de parametros con el obj @see android.os</li>
+ *     <li>Paso de mensages mediante la actividades de la clase <a href="https://developer.android.com/reference/android/content/Intent.html">Intent</a> </li>
+ * </ul>
+ * .Bundle;
+ */
 public class SendMensaggerActivity extends AppCompatActivity {
 
     private EditText edtMessage;
+    private EditText edtUser;
+    private Button btnOk;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send_mensagger);
+
+
+        //Iniciar los componetes
+        edtMessage = (EditText)findViewById(R.id.editMessage);
+        edtUser = (EditText)findViewById(R.id.editUser);
+        btnOk = (Button)findViewById(R.id.btnOk);
+
+        //1.Registrar un lisener o escuchar OnClickLisener
+        btnOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //1.Crear un objeto  bundle y añadirlo al message
+                Bundle bundle = new Bundle();
+                //2.recoger mensage
+                bundle.putString("message",edtMessage.getText().toString());
+                bundle.putString("user",edtUser.getText().toString());
+                //3.Crear un obj intent
+                Intent intent = new Intent(SendMensaggerActivity.this,ViewMesaggeActivity.class);
+                //4.Incluir bundel dentro de intent
+                intent.putExtras(bundle);
+                //5.Iniciar el activity view message
+                startActivity(intent);
+            }
+        });
+
+
     }
 
-    public void getonClick(View view) {
+   /** public void getonClick(View view) {
         switch (view.getId())
         {
             case R.id.btnOk:
-                //1.Crear un objeto  bundle y añadirlo al message
-                Bundle bundel = new Bundle();
+               //1.Crear un objeto  bundle y añadirlo al message
+                Bundle bundle = new Bundle();
                 //2.recoger mensage
-                bundel.putString("message",edtMessage.getText().toString());
+                bundle.putString("message",edtMessage.getText().toString());
+                bundle.putString("user",edtUser.getText().toString());
                 //3.Crear un obj intent
                 Intent intent = new Intent(SendMensaggerActivity.this,ViewMesaggeActivity.class);
-                intent.putExtra(bundel);
-                //4.Iniciar el activity view message
+                //4.Incluir bundel dentro de intent
+                intent.putExtras(bundle);
+                //5.Iniciar el activity view message
                 startActivity(intent);
                 break;
         }
-    }
+    }*/
 }
--- app/src/main/java/com/example/usuario/sendmensagge/ViewMesaggeActivity.java
+++ app/src/main/java/com/example/usuario/sendmensagge/ViewMesaggeActivity.java
@@ -2,12 +2,34 @@ package com.example.usuario.sendmensagge;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.widget.TextView;
+/**
+ * @Autor Juan Manuel Diaz Ortiz
+ * @Version v0.2
+ * @Comentario Esta clase recive un mensage de la clase SendMensaggerActivity y la muestra
+ */
 public class ViewMesaggeActivity extends AppCompatActivity {
+
+    private TextView txvViewMessage;
+    private TextView txvViewUser;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_mesagge);
+
+        //se enlaza con el editmessage de View
+        txvViewMessage = (TextView)findViewById(R.id.ediMessage);
+        txvViewUser = (TextView)findViewById(R.id.ediUserMessage);
+        //se recoge el intert y de el se coge el mensage que se envio con el bundle
+        txvViewMessage.setText(getIntent().getExtras().getString("message"));
+        String viewUser = String.format(getResources().getString(R.string.ediUserMessage),getIntent().getExtras().getString("user"));
+        txvViewUser.setText(viewUser);
+
+
+
+
+
     }
 }
