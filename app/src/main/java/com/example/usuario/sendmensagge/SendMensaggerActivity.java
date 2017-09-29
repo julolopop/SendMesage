307cdc47ba230dc5d60e2ddffe94c44c6d4aa3b5
juanma <julolopop@gmail.com>
2017-09-29 13:49:22 +0200

    Anadido la clase message con la implementacion parcel

--- app/src/main/java/com/example/usuario/sendmensagge/SendMensaggerActivity.java
+++ app/src/main/java/com/example/usuario/sendmensagge/SendMensaggerActivity.java
@@ -1,28 +1,35 @@
 package com.example.usuario.sendmensagge;
 
 import android.content.Intent;
+import android.nfc.Tag;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.example.usuario.sendmensagge.pojo.Message;
+
 /**
- * @author  Juan Manuel Diaz Ortiz
+ * @author Juan Manuel Diaz Ortiz
  * @version v0.2
- * <p>Esta clase manda un mensage a la clase ViewMesaggeActivity</p>
- *
- * <ul>
- *     <li>Concepto Context</li>
- *     <li>Paso de parametros con el obj @see android.os</li>
- *     <li>Paso de mensages mediante la actividades de la clase <a href="https://developer.android.com/reference/android/content/Intent.html">Intent</a> </li>
- * </ul>
- * .Bundle;
+ *          <p>Esta clase manda un mensage a la clase ViewMesaggeActivity</p>
+ *          <p>
+ *          <ul>
+ *          <li>Concepto Context</li>
+ *          <li>Paso de parametros con el obj @see android.os</li>
+ *          <li>Paso de mensages mediante la actividades de la clase <a href="https://developer.android.com/reference/android/content/Intent.html">Intent</a> </li>
+ *          </ul>
+ *          .Bundle;
  */
 public class SendMensaggerActivity extends AppCompatActivity {
 
     private EditText edtMessage;
     private EditText edtUser;
     private Button btnOk;
+    private static final String TAG = "com.example.usuario.sendmensagge";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,9 +38,9 @@ public class SendMensaggerActivity extends AppCompatActivity {
 
 
         //Iniciar los componetes
-        edtMessage = (EditText)findViewById(R.id.editMessage);
-        edtUser = (EditText)findViewById(R.id.editUser);
-        btnOk = (Button)findViewById(R.id.btnOk);
+        edtMessage = (EditText) findViewById(R.id.editMessage);
+        edtUser = (EditText) findViewById(R.id.editUser);
+        btnOk = (Button) findViewById(R.id.btnOk);
 
         //1.Registrar un lisener o escuchar OnClickLisener
         btnOk.setOnClickListener(new View.OnClickListener() {
@@ -42,10 +49,14 @@ public class SendMensaggerActivity extends AppCompatActivity {
                 //1.Crear un objeto  bundle y añadirlo al message
                 Bundle bundle = new Bundle();
                 //2.recoger mensage
-                bundle.putString("message",edtMessage.getText().toString());
-                bundle.putString("user",edtUser.getText().toString());
+                Message smg = new Message(edtMessage.getText().toString(),edtUser.getText().toString());
+                bundle.putParcelable("usuarioMenssage",smg);
+                /*
+                bundle.putString("message", edtMessage.getText().toString());
+                bundle.putString("user", edtUser.getText().toString());
+                */
                 //3.Crear un obj intent
-                Intent intent = new Intent(SendMensaggerActivity.this,ViewMesaggeActivity.class);
+                Intent intent = new Intent(SendMensaggerActivity.this, ViewMesaggeActivity.class);
                 //4.Incluir bundel dentro de intent
                 intent.putExtras(bundle);
                 //5.Iniciar el activity view message
@@ -54,24 +65,52 @@ public class SendMensaggerActivity extends AppCompatActivity {
         });
 
 
+        Log.d(TAG, "SendMensaggerActivity onCreate()");
     }
 
-   /** public void getonClick(View view) {
-        switch (view.getId())
-        {
-            case R.id.btnOk:
-               //1.Crear un objeto  bundle y añadirlo al message
-                Bundle bundle = new Bundle();
-                //2.recoger mensage
-                bundle.putString("message",edtMessage.getText().toString());
-                bundle.putString("user",edtUser.getText().toString());
-                //3.Crear un obj intent
-                Intent intent = new Intent(SendMensaggerActivity.this,ViewMesaggeActivity.class);
-                //4.Incluir bundel dentro de intent
-                intent.putExtras(bundle);
-                //5.Iniciar el activity view message
-                startActivity(intent);
-                break;
-        }
-    }*/
+    /**
+     * public void getonClick(View view) {
+     * switch (view.getId())
+     * {
+     * case R.id.btnOk:
+     * //1.Crear un objeto  bundle y añadirlo al message
+     * Bundle bundle = new Bundle();
+     * //2.recoger mensage
+     * bundle.putString("message",edtMessage.getText().toString());
+     * bundle.putString("user",edtUser.getText().toString());
+     * //3.Crear un obj intent
+     * Intent intent = new Intent(SendMensaggerActivity.this,ViewMesaggeActivity.class);
+     * //4.Incluir bundel dentro de intent
+     * intent.putExtras(bundle);
+     * //5.Iniciar el activity view message
+     * startActivity(intent);
+     * break;
+     * }
+     * }
+     */
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "SendMensaggerActivity onStart()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "SendMensaggerActivity OnResume()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "SendMensaggerActivity onStop()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "SendMensaggerActivity onPause()");
+    }
 }
--- app/src/main/java/com/example/usuario/sendmensagge/ViewMesaggeActivity.java
+++ app/src/main/java/com/example/usuario/sendmensagge/ViewMesaggeActivity.java
@@ -3,6 +3,7 @@ package com.example.usuario.sendmensagge;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import com.example.usuario.sendmensagge.pojo.Message;
 /**
  * @Autor Juan Manuel Diaz Ortiz
  * @Version v0.2
@@ -12,7 +13,7 @@ public class ViewMesaggeActivity extends AppCompatActivity {
 
     private TextView txvViewMessage;
     private TextView txvViewUser;
-
+    private Message smg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,9 +23,14 @@ public class ViewMesaggeActivity extends AppCompatActivity {
         //se enlaza con el editmessage de View
         txvViewMessage = (TextView)findViewById(R.id.ediMessage);
         txvViewUser = (TextView)findViewById(R.id.ediUserMessage);
+
         //se recoge el intert y de el se coge el mensage que se envio con el bundle
+        /*
         txvViewMessage.setText(getIntent().getExtras().getString("message"));
-        String viewUser = String.format(getResources().getString(R.string.ediUserMessage),getIntent().getExtras().getString("user"));
+        */
+        smg = ((Message)getIntent().getExtras().getParcelable("usuarioMenssage"));
+        txvViewMessage.setText( smg.get_message() );
+        String viewUser = String.format(getResources().getString(R.string.ediUserMessage),smg.get_user());
         txvViewUser.setText(viewUser);
 
 
